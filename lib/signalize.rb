a03f9ab8589b65e14f53bb052b01b237b8c02097
Jared White <jared@jaredwhite.com>
2023-03-07 16:09:53 -0800

    Signalize gem

---
+++ lib/signalize.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "signalize/version"
+
+module Signalize
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/signalize/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Signalize
+  VERSION = "0.1.0"
+end
